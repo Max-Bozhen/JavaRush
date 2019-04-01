7237155db839cbe82b7b8a5ab1bdd10740feb0e0
Storano <46112587+Storano@users.noreply.github.com>
2019-04-01 23:36:39 +0300

    Add files via upload

---
+++ task1910/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task19.task1910;
+
+/* 
+Пунктуация
+*/
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstFile = "";
+        String secondFile = "";
+        try{
+            firstFile = reader.readLine();
+            secondFile  = reader.readLine();
+            reader.close();
+        }catch (IOException ie){
+            System.out.println("Try again");
+        }
+        FileReader fr;
+        BufferedReader buffer;
+        FileWriter fw;
+        BufferedWriter writer;
+        ArrayList<String> fromFile =new ArrayList<>();
+        try{
+            fr = new FileReader(firstFile);
+            buffer = new BufferedReader(fr);
+            while (buffer.ready())
+                fromFile.add(buffer.readLine());
+            buffer.close();
+
+            fr.close();
+            fw = new FileWriter(secondFile, true);
+            writer = new BufferedWriter(fw);
+            for(String s : fromFile){
+                s = s.replaceAll("\\p{Punct}|\\n", "");
+                System.out.println(s);
+                writer.write(s);
+            }
+            writer.close();
+            fw.close();
+        }catch (IOException ie){
+
+        }
+    }
+}
